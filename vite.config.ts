c34e4a67e963d85925118942e6e703ab6db83e2d
ゆうもや <takex5g@users.noreply.github.com>
2024-08-20 01:20:56 +0900

    こう

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //docsのディレクトリを指定
+  base: '/serial-flash',
   build: {
     outDir: 'docs',
   },
