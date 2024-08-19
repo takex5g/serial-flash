70157fa9382a97a207b68e75e7a02d166a19d402
ゆうもや <takex5g@users.noreply.github.com>
2024-08-20 01:18:14 +0900

    しょうがない

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  //docsのディレクトリを指定
+  build: {
+    outDir: 'docs',
+  },
   //3000番
   server: {},
 });
