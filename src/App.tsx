dfd4236f3ef3029eedc7eeecc2fd897cdfa0712a
ゆうもや <takex5g@users.noreply.github.com>
2024-08-20 01:07:50 +0900

    一旦要らないの消した

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 import { MyAudio } from './utils/myAudio';
 const audio_ctx = new AudioContext();
@@ -15,65 +15,24 @@ const audios = [
   new MyAudio(audio_ctx, '/audio/7.mp3'),
 ];
 
-const c = new WebSocket('wss://d2mlz4dv52k4e2.cloudfront.net/websocket');
 function App() {
   const [serialPort, setSerialPort] = useState<any>(null);
-  const [lastFlashDate, setLastFlashDate] = useState<Date | null>(null);
-  const [isFlashing, setIsFlashing] = useState<boolean>(false);
-
-  let serialPort2: any = null;
-  useEffect(() => {
-    c.onopen = (event) => console.debug(event);
-    c.onerror = (error) => console.debug(error);
-    c.onmessage = (event) => {
-      console.log(event.data);
-      console.debug('message', serialPort);
-      writeSerial(serialPort);
-      setLastFlashDate(new Date());
-      setIsFlashing(true);
-
-      audios[Math.floor(Math.random() * 7)].play();
-      // datas = [...JSON.parse(event.data)];
-    };
-    c.onclose = () => {
-      console.debug('close');
-      c.close();
-    };
-  }, [serialPort]);
-
-  //lastFlashDateから10ms経過したらフラッシュを終了する
-  useEffect(() => {
-    if (lastFlashDate) {
-      const timer = setTimeout(() => {
-        setIsFlashing(false);
-      }, 10);
-      return () => clearTimeout(timer);
-    }
-  }, [lastFlashDate]);
-
-  const post = async () => {
-    await fetch('https://d2mlz4dv52k4e2.cloudfront.net/send_notification', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    // console.log(res);
-  };
+  const [isFlashing] = useState<boolean>(false);
 
   window.onload = function () {
     console.log('onload');
 
+    //@ts-ignore
     navigator.serial.addEventListener('connect', () => {
       // `e.target` に接続する、すなわち利用可能なポートのリストに加えます。
       // console.log(e.target);
     });
-
+    //@ts-ignore
     navigator.serial.addEventListener('disconnect', () => {
       // `e.target` を利用可能なポートのリストから外します。
       // console.log(e.target);
     });
-
+    //@ts-ignore
     navigator.serial.getPorts().then((ports: any) => {
       // ページの読み込み時、`ports` を用いて利用可能なポートのリストを初期化します。
       console.log(ports);
@@ -81,11 +40,11 @@ function App() {
   };
   const toConnect = async () => {
     console.log('connect serial port');
+    //@ts-ignore
     navigator.serial.requestPort().then((port: any) => {
       console.log(port);
       port.open({ baudRate: 9600 });
       setSerialPort(port);
-      serialPort2 = port;
     });
   };
 
